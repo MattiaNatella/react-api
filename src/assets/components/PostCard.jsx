04bbc1ec86bf814fa3166e072b3aef229ac5a1fb
MattiaNatella <mattianatella@gmail.com>
2025-01-22 22:08:50 +0100

    [add] published badge on postCard

--- src/assets/components/PostCard.jsx
+++ src/assets/components/PostCard.jsx
@@ -1,14 +1,14 @@
 
 
 const PostCard = (props) => {
-    const { id, title, content, image, tags } = props.post
+    const { id, title, content, image, tags, authorize } = props.post
     const handlerDeletePost = props.onDelete
     return (
         <div className="col-12 col-md-6 col-lg-4 my-2">
             <div className="card border border-success">
                 <img src={image} className="card-img-top" alt={title} />
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
+                    <h5 className="card-title">{title} {authorize ? <span class="badge text-bg-success">Pubblicato</span> : <span class="badge text-bg-warning">Bozza</span>}</h5>
                     <p className="card-text text-start">{content}</p>
                     <button
                         type="button"
