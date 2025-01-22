df12acf238d881a116f09b8e69744f706a631e18
MattiaNatella <mattianatella@gmail.com>
2025-01-22 15:25:09 +0100

    [add] placeholder + button elimina post

--- src/assets/components/PostCard.jsx
+++ src/assets/components/PostCard.jsx
@@ -5,10 +5,11 @@ const PostCard = (props) => {
     return (
         <div className="col-12 my-2">
             <div className="card border border-success">
-                <img src={image} className="card-img-top" alt={title} />
+                <img src='https://placehold.co/100x50' className="card-img-top" alt={title} />
                 <div className="card-body">
-                    <h5 class="card-title">{title}</h5>
+                    <h5 className="card-title">{title}</h5>
                     <p className="card-text text-start">{content}</p>
+                    <button type="button" className="btn btn-danger text-start"><i className="fa-solid fa-eraser text-dark pe-2"></i>Elimina post</button>
                 </div>
             </div>
         </div >
