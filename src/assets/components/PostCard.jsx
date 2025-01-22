bd1aaefb0fc9bc03bc5698a057a7a26d76d8d00d
MattiaNatella <mattianatella@gmail.com>
2025-01-22 23:10:24 +0100

    fix class to classname on postcard tags span

--- src/assets/components/PostCard.jsx
+++ src/assets/components/PostCard.jsx
@@ -7,16 +7,16 @@ const PostCard = (props) => {
 
     return (
         <div className="col-12 col-md-6 col-lg-4 my-2">
-            <div className="card border border-success">
+            <div className="card border border-danger-subtle">
                 <img src={image} className="card-img-top" alt={title} />
                 <div className="card-body">
-                    <h5 className="card-title">{title} {authorize ? <span class="badge text-bg-success">Pubblicato</span> : <span class="badge text-bg-warning">Bozza</span>}</h5>
+                    <h5 className="card-title">{title} {authorize ? <span className="badge text-bg-success">Pubblicato</span> : <span className="badge text-bg-warning">Bozza</span>}</h5>
                     {/* CONTENUTO POST */}
                     <p className="card-text text-start border border-3 border-danger rounded p-2">{content}</p>
                     <div className="row justify-content-between align-items-center ms-0">
                         {/* TAGS */}
                         <div className="col-6 position-relative border border-3 border-primary rounded-3 mt-3 pb-5">
-                            <h2 className="text-white border rounded bg-primary rounded-pill shadow p-2 mb-1 bg-body-primary rounded position-relative top-4 start-50 translate-middle p-0">Tags</h2>
+                            <h4 className="text-white border rounded bg-primary rounded-pill shadow p-2 mb-1 bg-body-primary rounded position-relative top-4 start-50 translate-middle p-0">Tags</h4>
                             {tags.map((tag, index) => (
                                 <span key={index} className="badge bg-primary mt me-1 pe-3">{tag}</span>
                             ))}
